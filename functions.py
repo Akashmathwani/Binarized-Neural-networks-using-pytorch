703e27e496bb2616bbe0e328f6818fdeb8abdea3
Akashmathwani <39944333+Akashmathwani@users.noreply.github.com>
2018-06-12 00:51:22 +0800

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 11 12:30:49 2018
+
+@author: akash
+"""
+
+import torch
+import torch.nn as nn
+from torch.autograd import Function
+
+
+class BinarizeF(Function):
+
+    @staticmethod
+    def forward(cxt, input):
+        output = input.new(input.size())
+        output[input >= 0] = 1
+        output[input < 0] = -1
+        return output
+
+    @staticmethod
+    def backward(cxt, grad_output):
+        grad_input = grad_output.clone()
+        return grad_input
+
+# aliases
+binarize = BinarizeF.apply
